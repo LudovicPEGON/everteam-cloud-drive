7246fbbacaf4e0b5acaf122a96cc4a242cfec2fd
ludovic pegon <l.pegon@everteam.com>
2017-04-14 15:18:13 +0200

    remove unnecessary deserializers to avoid exception during feign tests

--- everteam-cloud-drive-server/src/main/java/com/everteam/storage/jackson/StorageDeserializerModifier.java
+++ everteam-cloud-drive-server/src/main/java/com/everteam/storage/jackson/StorageDeserializerModifier.java
@@ -8,14 +8,14 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
 
 public class StorageDeserializerModifier extends BeanDeserializerModifier {
-    @SuppressWarnings("unchecked")
+    //@SuppressWarnings("unchecked")
     @Override
     public JsonDeserializer<?> modifyDeserializer(DeserializationConfig config, BeanDescription beanDesc,
             JsonDeserializer<?> deserializer) {
         if (beanDesc.getBeanClass() == ESRepository.class) {
-            return new ESRepositoryDeserializer((JsonDeserializer<Object>) deserializer);
+            //return new ESRepositoryDeserializer((JsonDeserializer<Object>) deserializer);
         } else if (beanDesc.getBeanClass() == ESFile.class) {
-            return new ESFileDeserializer((JsonDeserializer<Object>) deserializer);
+//            return new ESFileDeserializer((JsonDeserializer<Object>) deserializer);
         }
         return super.modifyDeserializer(config, beanDesc, deserializer);
     }
